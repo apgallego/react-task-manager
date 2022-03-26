420ec76a1a05a88f9ac302e8c842ba7612e90de8
apgallego <apgallego@users.noreply.github.com>
2022-03-26 21:32:07 +0100

    reminder, delete and part of the adding form

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,55 @@
-import Header from './components/Header';
-import Tasks from './components/Tasks';
-import { useState } from 'react';
+import { useState } from "react";
+import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
 
 const App = () => {
   const [tasks, setTasks] = useState([
     {
-        id: 1,
-        text: 'Cry',
-        day: 'Feb 5th at 2:30pm',
-        reminder: true,
+      id: 1,
+      text: "Cry",
+      day: "Feb 5th at 2:30pm",
+      reminder: true,
     },
     {
-        id: 2,
-        text: 'Cry more',
-        day: 'Feb 6th at 1:30pm',
-        reminder: true,
+      id: 2,
+      text: "Cry more",
+      day: "Feb 6th at 1:30pm",
+      reminder: true,
     },
     {
-        id: 3,
-        text: 'Cry even more',
-        day: 'Feb 7th at 4:30pm',
-        reminder: false,
-    }
+      id: 3,
+      text: "Cry even more",
+      day: "Feb 7th at 4:30pm",
+      reminder: false,
+    },
   ]);
+
+  //Delete Task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+
+  //Toggle Reminder
+  const toggleReminder = (id) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      )
+    );
+  };
 
   return (
     <div className="container">
       <Header />
-      <Tasks tasks={tasks}/>
+      <AddTask />
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
+      ) : (
+        "No tasks"
+      )}
     </div>
   );
-}
+};
 
 export default App;
